54f69603c3df9891d55649d94969c2efef2919d7
Mend1s <andremendesskt99@gmail.com>
2023-04-03 23:23:06 -0300

    Adicionar arquivos de projeto.

---
+++ ProductsApi/Controllers/ProductController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductsApi.Data;
+using ProductsApi.Models;
+
+namespace ProductsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {   
+        private readonly ProductsContext _context;
+
+        public ProductController(ProductsContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("/products")]
+        public async Task<ActionResult> GetProducts()
+        {
+            return Ok(await _context.Products.ToListAsync());
+        }
+
+        [HttpPost]
+        [Route("/products")]
+        public async Task<ActionResult> CreateProduct(Product product)
+        {
+            await _context.Products.AddAsync(product);
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("/products")]
+        public async Task<ActionResult> UpdateProduct(Product product)
+        {
+            // procuro na base pelo ID
+            var dbProduct = await _context.Products.FindAsync(product.Id);
+
+            // verifico se o ID é nulo
+            if(dbProduct == null) { return NotFound(); }
+
+            dbProduct.Name = product.Name;
+            dbProduct.Price = product.Price;
+            dbProduct.Category = product.Category;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(product);
+        }
+
+        [HttpDelete]
+        [Route("/products")]
+        public async Task<ActionResult> DeleteProduct(Guid id)
+        {
+            var dbProduct = await _context.Products.FindAsync(id);
+
+            // verifico se o ID é nulo
+            if (dbProduct == null) { return NotFound(); }
+
+            _context.Products.Remove(dbProduct);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ ProductsApi/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using ProductsApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services
+    .AddDbContext<ProductsContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ServerConnection")));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
